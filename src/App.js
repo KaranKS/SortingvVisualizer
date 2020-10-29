cc466466a751ed8b337d4e62bed40eba0671dd51
Karan Singh <ksingh1@cs.iitr.ac.in>
2020-10-29 12:45:30 +0530

    merge sort is completed

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SortingVisualizer from './SortingVisualizer/SortingVisualizer.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <SortingVisualizer></SortingVisualizer>
+    </div>
+  );
+}
+
+export default App;
